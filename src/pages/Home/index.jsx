88e061ba7d446772dd6c83a2ea51fc3516b743ed
xiedidan <xiedidan@yeah.net>
2019-02-28 12:39:34 +0800

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+// antd component
+import { DatePicker, Input } from 'antd';
+
+// mobx observer
+import { observer } from 'mobx-react';
+
+// mobx store
+import appStore from '../../stores/AppStore';
+
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+
+import IntroBanner from './components/IntroBanner';
+import AblityItems from './components/AblityItems';
+import IntroTab from './components/IntroTab';
+import CardItems from './components/CardItems';
+import SlideBanner from './components/SlideBanner';
+
+@observer
+export default class Home extends Component {
+  static displayName = 'Home';
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  onDateChange(date, dateString) {
+    appStore.selectDate(date);
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <DatePicker onChange={this.onDateChange} />
+        <Input placeholder="Date from Mobx" value={appStore.today} />
+        <Input placeholder="Next day" value={appStore.tomorrow} />
+        <Header />
+        <IntroBanner />
+        <AblityItems />
+        <IntroTab />
+        <CardItems />
+        <SlideBanner />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    width: '100%',
+    minWidth: '1280px',
+  },
+};
